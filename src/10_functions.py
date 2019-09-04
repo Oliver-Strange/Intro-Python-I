36326e8351e1e01e2ad4ff969158020c2ac15b95
Oliver-Strange <preston.burton@gmail.com>
2019-09-04 16:00:19 -0700

    comprehensions, dictionaries, functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,27 @@
 
 # YOUR CODE HERE
 
+
+def evenNum(num):
+    if (num % 2) == 0:
+        return True
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+# print(evenNum(num))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+
+def evenOdd(num):
+    if (num % 2) == 0:
+        print('Even!')
+    else:
+        print("Odd")
+
+
+print(evenOdd(num))
